e87f9611a45f59daa56b128c77fa8e29547ba3c9
Memnoc <smartdroidesign@gmail.com>
2026-04-02 10:52:33 +0200

    hit logic and collision shapes set up (hopefully) correctly

--- scene/character/player/chopping_state.gd
+++ scene/character/player/chopping_state.gd
@@ -2,6 +2,7 @@ extends NodeState
 
 @export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
+@export var hit_component_collision_shape: CollisionShape2D
 
 const DIRECTION_ANIMATIONS: Dictionary = { 
 	Vector2.LEFT: "chopping_left",
@@ -9,6 +10,10 @@ const DIRECTION_ANIMATIONS: Dictionary = {
 	Vector2.UP: "chopping_back",
 	Vector2.DOWN: "chopping_front",
 }
+
+func _ready() -> void:
+	hit_component_collision_shape.disabled = true
+	hit_component_collision_shape.position = Vector2(0, 0)
 
 func _on_process(_delta : float) -> void:
 	pass
@@ -23,6 +28,16 @@ func _on_next_transitions() -> void:
 
 func _on_enter() -> void:
 	animated_sprite_2d.play(DIRECTION_ANIMATIONS.get(player.player_direction, "chopping_front"))
+	if player.player_direction == Vector2.DOWN:
+		hit_component_collision_shape.position = Vector2(9,0)
+	elif player.player_direction == Vector2.LEFT:
+		hit_component_collision_shape.position = Vector2(-9, 0)
+	elif player.player_direction == Vector2.UP:
+		hit_component_collision_shape.position = Vector2(0, -18)
+	elif player.player_direction == Vector2.DOWN:
+		hit_component_collision_shape.position = Vector2(0, 3)
+	hit_component_collision_shape.disabled = false
 
 func _on_exit() -> void:
 	animated_sprite_2d.stop()
+	hit_component_collision_shape.disabled = true
